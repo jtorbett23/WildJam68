d56951210fe89e11d0e29e562af56aee6646314c
jtorbett23 <56300912+jtorbett23@users.noreply.github.com>
2024-04-13 12:09:10 +0100

    only draw on canvas

--- draw.gd
+++ draw.gd
@@ -1,29 +1,43 @@
 extends Node2D
 
-var color = Color(255, 255, 255, 1)
-var radius = 0
+var color = Color.BLACK	
+var radius = 20
 
+var draw_data :Array = []
+var min_pos : Vector2
+var max_pos : Vector2
 
 func _draw():
-	if Input.is_action_pressed("draw"):
-		RenderingServer.viewport_set_clear_mode(get_viewport().get_viewport_rid(), RenderingServer.VIEWPORT_CLEAR_NEVER)
-		radius = 20
-	if Input.is_action_just_released("draw"):
-		radius = 0.0
-
-
-	draw_circle(Vector2.ZERO, radius, color)
+	for pos in draw_data:
+		draw_circle(pos, radius, color)
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 
 
+	var background : Sprite2D = $"Background"
+	var background_size = background.get_rect().size * background.transform.get_scale()
+	min_pos = background.global_position
+	max_pos = min_pos + background_size
+	print(min_pos, max_pos)
 
 	pass # Replace with function body.
 
 
+func is_on_canvas(mouse_pos) -> bool:
+	#within min
+	if (mouse_pos.x > min_pos.x) and (mouse_pos.y > min_pos.y):
+		#within max
+		if (mouse_pos.x < max_pos.x) and (mouse_pos.y < max_pos.y):
+			return true
+	return false
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	position = get_global_mouse_position()
+	var pos = get_global_mouse_position()
+	if Input.is_action_pressed("draw") and is_on_canvas(pos):
+		draw_data.append(pos)
+	elif Input.is_action_just_pressed("clear"):
+		draw_data = []
 	queue_redraw()
 	pass
--- main.gd
+++ main.gd
@@ -4,11 +4,7 @@ extends Node2D
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
-
-
-
-
-
+	
 #Take screenshoot image
 func take_pic(filename=""):
 	var capture = get_viewport().get_texture().get_image()
@@ -21,7 +17,3 @@ func take_pic(filename=""):
 func _process(_delta):
 	if Input.is_action_just_pressed("screenshot"):
 		take_pic("Input")
-	elif Input.is_action_just_pressed("clear"):
-		RenderingServer.viewport_set_clear_mode(get_viewport().get_viewport_rid(), RenderingServer.VIEWPORT_CLEAR_ALWAYS)
-
-
