88be39cbfab4104ed0251019043b7652ad971d10
jtorbett23 <56300912+jtorbett23@users.noreply.github.com>
2024-04-13 12:39:25 +0100

    more robust undo

--- draw.gd
+++ draw.gd
@@ -4,12 +4,13 @@ var color = Color.BLACK
 var radius = 20
 
 var draw_data : Array = []
+var previous_draw : Array = [[]]
 var min_pos : Vector2
 var max_pos : Vector2
 
 func _draw():
-	for pos in draw_data:
-		draw_circle(pos, radius, color)
+	for data in draw_data:
+		draw_circle(data.pos, data.radius, color)
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -36,10 +37,22 @@ func is_on_canvas(mouse_pos) -> bool:
 func _process(delta):
 	var pos = get_global_mouse_position()
 	if Input.is_action_pressed("draw") and is_on_canvas(pos):
-		draw_data.append(pos)
+		if(draw_data.find({"pos":pos, "radius": radius}) == -1):
+			draw_data.append({"pos":pos, "radius": radius})
+			#undo v2
+			previous_draw.append(draw_data.duplicate())
 	elif Input.is_action_just_pressed("clear"):
 		draw_data = []
+		#undo v2
+		previous_draw.append([])
 	elif Input.is_action_pressed("undo"):
-		draw_data.pop_back()
+		#undo v2
+		if(previous_draw.size() > 0):
+			var prev = previous_draw.pop_back()
+			draw_data = prev
+		elif(previous_draw.size() == 0):
+			previous_draw = [[]]
+		#undo v1
+		# draw_data.pop_back()
+			
 	queue_redraw()
-	pass
