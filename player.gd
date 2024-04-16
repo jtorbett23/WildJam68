5c7e3a877867797772da88d92e04c444bd72f5f9
jtorbett23 <56300912+jtorbett23@users.noreply.github.com>
2024-04-16 22:57:00 +0100

    add interact when in area

--- player.gd
+++ player.gd
@@ -2,13 +2,28 @@ extends CharacterBody2D
 
 @export var speed : int = 400
 
+@onready var area : Area2D = $"../Area2D"
+
+var interactable = false
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	area.connect("body_entered", on_area_entered)
+	area.connect("body_exited", on_area_exited)
 	pass # Replace with function body.
+
+func on_area_entered(_event):
+	print("yo")
+	interactable = true
+
+func on_area_exited(_event):
+	print("no")
+	interactable = false
 
 func _input(event):
 	if event.is_action_pressed("interact"):
-		print("interact")
+		if(interactable):
+			print("interact")
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -22,4 +37,6 @@ func _physics_process(delta):
 		velocity.x = -speed
 	else:
 		velocity.x = 0
-	move_and_collide(velocity * delta)
+	var collision = move_and_collide(velocity * delta)
+	# if collision:
+	# 	print("I collided with ", collision.get_collider().name)
