c34a757f3404c47916964392a9271decafcd82ba
jtorbett23 <56300912+jtorbett23@users.noreply.github.com>
2024-04-17 23:59:43 +0100

    use canvas layer for transition

--- camera.gd
+++ camera.gd
@@ -3,11 +3,13 @@ extends Camera2D
 var target = null
 var transition_ref = preload("res://transition.tscn")
 var transition
-
+var canvas 
 
 func _ready():
+	canvas = CanvasLayer.new()
+	add_child(canvas)
 	transition = transition_ref.instantiate()
-	add_child(transition)
+	canvas.add_child(transition)
 
 func set_follow(new_target):
 	anchor_mode = Camera2D.ANCHOR_MODE_DRAG_CENTER
